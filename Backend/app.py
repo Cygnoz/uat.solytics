2d31bb0d08b51d3c00a75c467a0a754d3942fbb8
AkhilMSajeevan <akhilmsajeevan14@gmail.com>
2025-04-02 12:36:52 +0530

    UAT

--- Backend/app.py
+++ Backend/app.py
@@ -62,7 +62,7 @@ chatbots_collection = db["chatbots"]
 
 @app.route("/", methods=["GET"])
 def status():
-    return jsonify({"message": "Chatbot SIT is working 🤖"}), 200
+    return jsonify({"message": "Chatbot UAT is working 🤖"}), 200
 
 # def process_scraped_data(text):
 #     """Process text data and create FAISS index"""
